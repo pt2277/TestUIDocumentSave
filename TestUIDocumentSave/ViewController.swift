c4bb3c65cb158568e284d2750c7b5953befed46a
Papoj Thamjaroenporn <papojt@papojsmacbookpro.local>
2017-12-30 22:58:36 +0700

    Initial Commit

---
+++ TestUIDocumentSave/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestUIDocumentSave
+//
+//  Created by Papoj Thamjaroenporn on 12/30/17.
+//  Copyright © 2017 Papoj Thamjaroenporn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
